eb36858079304a6a737eae4d532ce065a54abc11
casparjvanwijk <52788295+casparjvanwijk@users.noreply.github.com>
2022-11-14 23:09:27 +0100

    Ask port

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Start");
+#!/usr/bin/env node
+
+console.log("hello");
+import * as readline from "node:readline/promises";
+
+const DEFAULT_PORT = 3000;
+
+var rl = readline.createInterface(process.stdin, process.stdout);
+const port =
+  (await rl.question(
+    `On which port is your application running (default: ${DEFAULT_PORT})?`
+  )) || DEFAULT_PORT;
+
+// TODO:
+// Get local ip
+// Create link (ip:port)
+// Create QR
+// Output QR
