81d4ae2c89b6ad7fd8e2170737d4ccfb7c5babc6
casparjvanwijk <52788295+casparjvanwijk@users.noreply.github.com>
2022-11-14 23:35:55 +0100

    Accept port as command line argument

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,17 @@
 #!/usr/bin/env node
 
-import * as readline from "node:readline/promises";
 import ip from "ip";
 import QRCode from "qrcode";
 
 const DEFAULT_PORT = 3000;
 
-var rl = readline.createInterface(process.stdin, process.stdout);
-
-const port =
-  (await rl.question(
-    `On which port is your application running (default: ${DEFAULT_PORT})?`
-  )) || DEFAULT_PORT;
-
+const argPort = process.argv[2];
+const port = argPort || DEFAULT_PORT;
 const ipAddress = ip.address();
-
 const url = `http://${ipAddress}:${port}`;
+const QR = await QRCode.toString(url);
 
-const QR = await QRCode.toString(url, { type: "terminal" });
-
-console.log(`\nQRCode for ${url}:`);
+console.log(`QR code for ${url}:`);
 console.log(QR);
 
 process.exit();
