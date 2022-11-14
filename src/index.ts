074907d58eff8e7f7aa64f3d5284d0111105cf2c
casparjvanwijk <52788295+casparjvanwijk@users.noreply.github.com>
2022-11-14 23:16:54 +0100

    Get ip address and create url

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 #!/usr/bin/env node
 
-console.log("hello");
 import * as readline from "node:readline/promises";
+import ip from "ip";
 
 const DEFAULT_PORT = 3000;
 
 var rl = readline.createInterface(process.stdin, process.stdout);
+
 const port =
   (await rl.question(
     `On which port is your application running (default: ${DEFAULT_PORT})?`
   )) || DEFAULT_PORT;
 
+const ipAddress = ip.address();
+
+const url = `https://${ipAddress}:${port}`;
+
+console.log("url", url);
+
 // TODO:
-// Get local ip
-// Create link (ip:port)
 // Create QR
 // Output QR
